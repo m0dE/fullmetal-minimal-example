ffef7dea748f8db42569fe666d7f5760b336a1bf
shaileshminsnapsys <shailesh.m.insnapsys@gmail.com>
2023-12-18 12:24:52 +0530

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,6 @@
+// export const FullmetalAPIURL = 'https://api.fullmetal.ai';
+export const FullmetalAPIURL = 'http://localhost:8081';
+// export const ChatBackendScoketUrl = 'https://chat.fullmetal.ai/';
+export const ChatBackendScoketUrl = 'http://localhost:5025/';
+export const FullmetalChatApiUrl = 'http://localhost:8000';
+export const FullmetalApiKey = '';
